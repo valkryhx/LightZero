1ae8d9b7d84936cc9bf4081504b72e70198fd672
valkryhx <283306449@qq.com>
2024-03-02 16:11:45 +0800

    123

--- zoo/classic_control/mazegame/config/eval_mymaze_muzero_config.py
+++ zoo/classic_control/mazegame/config/eval_mymaze_muzero_config.py
@@ -98,15 +98,16 @@ if __name__ == "__main__":
         from lzero.entry import eval_muzero_with_gym_env as eval_muzero
 
     #train_muzero([main_config, create_config], seed=0, max_env_step=max_env_step)
-    if len(sys.argv)>1:
-        print(sys.argv[1])
+    if len(sys.argv)>0:
+        print(sys.argv)
         res = eval_muzero(
             input_cfg=[main_config, create_config],
             seed= 0,
             model= None,
+            print_seed_details= False,
             model_path =sys.argv[1], '/kaggle/working/LightZero/data_mz_ctree/mymaze_muzero_ns25_upc100_rr0_seed0_240301_065201/ckpt/ckpt_best.pth.tar',
-            num_episodes_each_seed= 1,
-            print_seed_details= False)
+            num_episodes_each_seed= 1
+            )
         print(res)
     else :
         print(f"请传入pth.ckt.tar  path")
