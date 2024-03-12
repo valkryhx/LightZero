22a61a7382feafbc118f0d1ac6902aca1d114f95
valkryhx <283306449@qq.com>
2024-03-12 09:40:49 +0800

    123

--- lzero/mcts/tests/config/mazegame_muzero_config_for_test.py
+++ lzero/mcts/tests/config/mazegame_muzero_config_for_test.py
@@ -39,7 +39,8 @@ mymaze_muzero_config = dict(
         sampled_algo=False,
         gumbel_algo=False,
         use_ture_chance_label_in_chance_encoder=False,
-        model_path="/kaggle/working/LightZero/data_mz_ctree/mymaze_muzero_nsadadasd20_upc100_rr0_seed0/ckpt/ckpt_best.pth.tar" ,
+        # add moel_path here, but this path seems not working, I can even place a wrong path here , the test process still runs.
+        model_path="/kaggle/working/LightZero/data_mz_ctree/mymaze_muzero_ns25_upc100_rr0_seed0/ckpt/ckpt_best.pth.tar" ,
         # add end
         model=dict(
             observation_shape=(1,4,4),#16,#4,
