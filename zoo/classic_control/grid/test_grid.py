f33572383388ee508fb94d853bd3644342b10f65
valkryhx <283306449@qq.com>
2024-03-13 19:49:32 +0800

    增加efficientzero mazegame config

---
+++ zoo/classic_control/grid/test_grid.py
@@ -0,0 +1,47 @@
+
+import gymnasium as gym
+import numpy as np
+import pygame
+#Maze config
+from zoo.classic_control.grid.grid.grid import GridEnv
+
+from gymnasium.envs.registration import register
+
+register(
+     id="MyGrid-v1",
+     entry_point="zoo.classic_control.grid.grid.grid:GridEnv",
+     max_episode_steps=5,#300,
+)
+
+#maze = [
+#    ['S', '', '.', '.'],
+#    ['.', '#', '.', '#'],
+#    ['.', '.', '.', '.'],
+#    ['#', '.', '#', 'G'],
+#]
+# Test the environment
+env = gym.make('MyGrid-v1')
+obs = env.reset()
+env.render()
+print(f'h_score={env.h_score}')
+done = False
+i=0
+print(f'\n\nSTART TO PLAY====\n')
+while i<10:
+    #pygame.event.get()
+    
+    #action = env.action_space.sample()  # Random action selection
+    action = env.random_action()
+    print(f'idx={i}, action={action}')
+    obs, reward, done, _,_ = env.step(action)
+    print(f'h_score={env.h_score}')
+    env.render()
+    print('Reward:', reward)
+    print('Done:', done)
+    
+    #pygame.time.wait(200)  
+    i += 1
+    if done:
+        
+        break
+    
