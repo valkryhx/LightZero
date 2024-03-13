b00457ffa0d8bf6b8b1a7fcb4432220cdf02b689
valkryhx <283306449@qq.com>
2024-03-13 17:37:40 +0800

    增加efficientzero mazegame config

---
+++ zoo/classic_control/grid/envs/test_env_grid_here.py
@@ -0,0 +1,41 @@
+
+import gymnasium as gym
+import numpy as np
+import pygame
+#Maze config
+from zoo.classic_control.grid.grid.grid import GridEnv
+from zoo.classic_control.grid.envs.grid_lightzero_env import MyGridEnv
+from gymnasium.envs.registration import register
+
+
+
+
+
+
+
+#maze = [
+#    ['S', '', '.', '.'],
+#    ['.', '#', '.', '#'],
+#    ['.', '.', '.', '.'],
+#    ['#', '.', '#', 'G'],
+#]
+# Test the environment
+env = MyGridEnv()
+obs = env.reset()
+env.render()
+
+done = False
+i=0
+print(f'\n\n====START TO PLAY====\n')
+while i<10:
+    #pygame.event.get()
+    
+    action = env.random_action()
+    print(f'idx={i}, action={action}')
+    obs, rew, done, info = env.step(action)
+    env.render()
+    print('Reward:', reward)
+    print('Done:', done)
+    
+    #pygame.time.wait(200)  
+    i += 1
