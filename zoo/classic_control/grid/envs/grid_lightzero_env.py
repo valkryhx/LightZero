111e8b17b3062147984e0b19b2cd67febf308527
valkryhx <283306449@qq.com>
2024-04-23 19:07:56 +0800

    Update grid_lightzero_env.py

--- zoo/classic_control/grid/envs/grid_lightzero_env.py
+++ zoo/classic_control/grid/envs/grid_lightzero_env.py
@@ -103,8 +103,8 @@ class MyGridEnv(BaseEnv):
         #action_mask = np.ones(self.action_space.n, 'int8')
         # 参考 gomoku_env.py的定义
         action_mask = np.zeros(grid_size*grid_size, 'int8')
-        action_mask[self._env.legal_actions()] = 1 # 
-        #print(f'self.legal_actions={self._env.legal_actions()}')
+        action_mask[self.legal_actions] = 1 # 
+        #print(f'self.legal_actions={self.legal_actions}')
         obs = {'observation': obs, 'action_mask': action_mask, 'to_play': -1}
 
         return obs
@@ -141,7 +141,7 @@ class MyGridEnv(BaseEnv):
         #action_mask = np.ones(self.action_space.n, 'int8')
         # 参考 gomoku_env.py的def _player_step定义 实时更新action_mask 
         action_mask = np.zeros(grid_size*grid_size, 'int8')
-        action_mask[self._env.legal_actions()] = 1 # 
+        action_mask[self.legal_actions] = 1 # 
         obs = {'observation': obs, 'action_mask': action_mask, 'to_play': -1}
 
         return BaseEnvTimestep(obs, rew, done, info)
@@ -176,7 +176,7 @@ class MyGridEnv(BaseEnv):
          """
         # 在legal actions中随机选一个 而不是从所有actions中随机选
         # 参考 gomoku_env.py
-        action_list = self._env.legal_actions()
+        action_list = self.legal_actions
         return np.random.choice(action_list)
         #random_action = self.action_space.sample()
         #random_action = to_ndarray([random_action], dtype=np.int64)
